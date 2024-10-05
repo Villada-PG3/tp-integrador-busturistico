98a66e33a5a5d12e0b03647001e7eaccfcbf53b5
joacochayle1 <110994640+joacochayle1@users.noreply.github.com>
2024-10-05 12:55:15 -0300

    agrego los url de recorrido y parada

--- appbus/urls.py
+++ appbus/urls.py
@@ -3,4 +3,6 @@ from .views import IndexView,BaseView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('base/',BaseView.as_view(), name='base'),
+    path('recorrido/',RecorridoView.as_view(), name='recorrido'),
+    path('parada/<int:id>/',ParadaView.as_view(), name='parada'),
 ]
